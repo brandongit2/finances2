924a7580e986387b5a5b41cf46dcb7bd6b782302
Brandon Tsang <brandononline2@gmail.com>
2022-12-15 20:01:58 -0500

    add assets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,7 @@
 <script lang="ts">
-	let name = ``
-
-	const handleChange = (e: Event & {currentTarget: EventTarget & HTMLFormElement}) => {
-		console.log(e.currentTarget.dataset)
-	}
-
-	const handleSubmit = (e: SubmitEvent & {currentTarget: EventTarget & HTMLFormElement}) => {
-		e.preventDefault()
-		// console.log(e.currentTarget.da)
-	}
+	import logo from "$lib/assets/slate-logo.svg"
 </script>
 
-<div>
-	<form on:submit={(e) => void handleSubmit(e)} on:change={(e) => {}}>
-		<input bind:value={name} />
-		<p>{name}</p>
-		<button type="submit">Submit</button>
-	</form>
-</div>
+<img src={logo} alt="Slate logo" class="h-12" />
+
+<p>Welcome to Slate! This project is in its early beginnings, so there isn't much content yet.</p>
